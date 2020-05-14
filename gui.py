79537e7761463110d52a8f5ae520fa254e553a0c
Florian9901 <peche.florian@live.fr>
2020-05-14 15:23:01 +0200

    Question 4 terminé

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction
 
 
 class Gui(QMainWindow):
@@ -7,7 +7,23 @@ class Gui(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Menu
+        self.menu = self.menuBar()
+        self.file_menu = self.menu.addMenu("Fichier")
         self.initUI()
+
+        # Exit QAction
+        open_action = QAction("Ouvrir", self)
+        save_action = QAction("Enregistrer", self)
+        exit_action = QAction("Quitter", self)
+        open_action.setStatusTip('Ouvrir un ficher')
+        save_action.setStatusTip('Sauvegarder le fichier')
+        exit_action.setStatusTip('Quitter la fenêtre')
+        self.file_menu.addAction(open_action)
+        self.file_menu.addAction(save_action)
+        self.file_menu.addAction(exit_action)
+
+
 
     def initUI(self):
         self.statusBar().showMessage('Ready')
@@ -16,3 +32,5 @@ class Gui(QMainWindow):
         self.setWindowTitle('Jeux')
         self.resize(1280, 720)
         self.show()
+
+
